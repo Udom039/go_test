94f0957ec1219415a35a88c2d762b08d68829571
Udom Neakaew <udom.neakaew2538@gmail.com>
2019-06-11 13:55:08 +0700

    create_golang

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+import "encoding/json"
+
+type customer struct {
+	Firstname string
+	Lastname  string
+	Code      int
+	Phone     string
+}
+
+func main() {
+	customerLists := []customer{}
+	customer1 := customer{
+		Firstname: "Chaiyarin",
+		Lastname:  "Niamsuwan",
+		Code:      111990,
+		Phone:     "085661234",
+	}
+	customer2 := customer{
+		Firstname: "Atikom",
+		Lastname:  "Sombutjalearn",
+		Code:      111991,
+		Phone:     "085664321",
+	}
+	customer3 := customer{
+		Firstname: "Kritsana",
+		Lastname:  "Punyaphon",
+		Code:      111992,
+		Phone:     "085662344",
+	}
+	customerLists = append(customerLists, customer1)
+	customerLists = append(customerLists, customer2)
+	customerLists = append(customerLists, customer3)
+	customerListsJson, err := json.Marshal(customerLists) // Marshal... Array to JSON
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(customerListsJson))
+}
